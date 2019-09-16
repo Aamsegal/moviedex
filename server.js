62a470dba214508b1532bb8eb364441566338938
Amsegal93 <Amsegal93@gmail.com>
2019-09-16 15:38:51 -0400

    Fixed syntax errors as well as the bearer token error with postman

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const apiToken = process.env.API_TOKEN
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
-  
     if (!authToken || (authToken && authToken.split(' ')[1] !== apiToken)) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
@@ -55,7 +54,7 @@ app.get('/movies', function handleGetMovies(req, res) {
 
     if (req.query.avg_vote) {
         response = response.filter(movie =>
-            Number(movies.avg_vote) >= Number(req.query.avg_vote)
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
             ) 
         }
 
